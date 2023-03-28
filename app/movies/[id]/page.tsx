049229687062c8d38cc7da42065e2dccc58e9409
Grégory <contact@gregory-gerard.dev>
2023-03-28 22:49:11 +0200

    fix: seo

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -4,9 +4,7 @@ import prisma from '@/prisma/prisma';
 import { notFound } from 'next/navigation';
 import { getMovie } from '@/services/tmdb/movies';
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const id = +params.id;
-
+async function retrieveMovieAndThrowIfNotFound(id: number) {
   const movie = await prisma.movie.findUnique({ where: { id } });
 
   // If movie don't have m3u8 then it's not published already
@@ -14,21 +12,26 @@ export default async function Page({ params }: { params: { id: string } }) {
     notFound();
   }
 
-  let movieWithTMDBData;
-
   try {
-    movieWithTMDBData = await getMovie(movie.id);
+    return { movie, tmdb: await getMovie(movie.id) };
   } catch (e) {
     notFound();
   }
+}
+
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const { tmdb } = await retrieveMovieAndThrowIfNotFound(+params.id);
+
+  return {
+    title: tmdb.title,
+  };
+}
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const { movie, tmdb } = await retrieveMovieAndThrowIfNotFound(+params.id);
 
   return (
-    <Player
-      id={movie.id}
-      src={movie.m3u8}
-      title={movieWithTMDBData.title}
-      poster={movieWithTMDBData.images.backdrops[0].file_path || ''}
-    />
+    <Player id={movie.id} src={movie.m3u8!} title={tmdb.title} poster={tmdb.images.backdrops[0].file_path || ''} />
   );
 }
 

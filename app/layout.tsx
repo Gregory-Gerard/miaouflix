9b9535d07fee9f0b221e092d0fc2328f06dc3689
Grégory <contact@gregory-gerard.dev>
2023-07-09 21:35:34 +0200

    feat: add umami tracker

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 import '@/app/globals.css';
 
 const inter = Inter({
@@ -24,6 +25,13 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr" className={inter.variable}>
+      {process.env.NODE_ENV === 'production' && (
+        <Script
+          data-website-id="92cc1168-863c-45f6-a0ec-ab13c9543099"
+          src="https://umami.gregory-gerard.dev:81/630abdb504d2a.js"
+        />
+      )}
+
       <body className="min-h-full overflow-x-hidden bg-neutral-950 text-neutral-50">
         <main>{children}</main>
       </body>
